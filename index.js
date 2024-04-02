ebace239d342dc26c177779ce6980e2498a85a3c
Ayan-Singh <ayansingh2042@gmail.com>
2024-04-02 22:13:06 +0530

    2nd day

--- index.js
+++ index.js
@@ -10,11 +10,14 @@
 //if the input string is empty or contain only whitespaces the function should return an false 
 // the function should ignore leading and trailing whitespace when determining the longest word 
 
-function findLongestWord(str){
-    if(str.trim().length===0){
-        return false ;
-    }
-    let str1 = str.trim().split(/\s+/);
+// function findLongestWord(str){
+//     if(str.trim().length===0){
+//         return false ;
+//     }
+//     let str1 = str.trim().split(/\s+/);
+
+    //*********** */
+
     // let longestWord = "";
     // for(let i of str1){
     //     if(i.length>longestWord.length){
@@ -23,6 +26,7 @@ function findLongestWord(str){
 
     // }
     // return longestWord ; 
+
 
     //****OR     */
 
@@ -33,6 +37,56 @@ function findLongestWord(str){
 
     //**************OR*******
 
-    return str1.reduce((accum,curWord)=>(curWord.length > accum.length ? curWord :accum),"" );
+    // return str1.reduce((accum,curWord)=>(curWord.length > accum.length ? curWord :accum),"" );
+
+    
+// }
+// console.log(findLongestWord(" Ayaan is Very Goooood boy and heee "));
+
+
+
+//-------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+//HASHTAG GENERATOR -----
+
+//You are required to implement a function generateHash that generates a hash tag from given input string. The hash tag should be constructed as follows : 
+
+//The input string should be converted to a hash tag format , where each word is capitalized and concatenated together without spaces. 
+//if the length of input string is greater than 280 character or if the input string is empty orcontains only whitespaces the function should return false 
+//Otherwise the function should return the generated hash tag prefixed with #
+
+// function generateHash(str){
+//     if(str.length > 280 || str.trim().length === 0){
+//         return false ;    
+//     }
+
+//     str = str.trim().split(" ");   //foreach me value return nhi kr skte 
+//     str = str.map((curElem) => 
+//         curElem.replace(curElem[0], curElem[0].toUpperCase() ) 
+//     );
+//     str=`#${str.join("")}`;
+
+
+//     return str ;
+    
+// }
+// console.log(generateHash("my name is ayan Singh "));
+
+
+function generateHash(str){
+    if(str.length > 280 || str.trim().length === 0){
+        return false ;    
+    }
+
+    str = str.trim().split(" ");   //foreach me value return nhi kr skte 
+    str = str.map((curElem) => 
+        // curElem.replace(curElem[0], curElem[0].toUpperCase() ) 
+        curElem.charAt(0).toUpperCase() + curElem.slice(1)
+    );
+    str=`#${str.join("")}`;
+
+
+    return str ;
+    
 }
-console.log(findLongestWord(" Ayaan is Very Goooood boy and heee "));
+console.log(generateHash("my name is ayan Singh")) ; 
